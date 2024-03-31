4c71d91a3fefff351b80ee0ec81899ab2c35b2c8
Shailesh Nagpure <159604979+aladinx98@users.noreply.github.com>
2024-03-31 11:42:57 +0530

    update mongo url

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ const DB_NAME = 'neel'; // Replace with your actual database name
 // Construct the connection string with the username, password, and database name
 // const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.sxpngpr.mongodb.net/${DB_NAME}`;
 // let connectoin = mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.sxpngpr.mongodb.net/${DB_NAME}`) ;
-const connectoin = mongoose.connect(`mongodb+srv://surjeshyadav:m275os2o9W6t3c5b@cluster0.25agdww.mongodb.net/`)
+// const connectoin = mongoose.connect(`mongodb+srv://surjeshyadav:m275os2o9W6t3c5b@cluster0.25agdww.mongodb.net/`)
+const connectoin = mongoose.connect(`mongodb+srv://fxst:tO48h7yzJGdasx5z@cluster0.y61tyw7.mongodb.net/`)
 
 export default connectoin ;
